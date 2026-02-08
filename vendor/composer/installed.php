fdae3daa1854ed97fe26066cfa27527162b64de8
TSU Admin <admin@tsuniversity.edu.ng>
2026-02-08 12:03:26 +0100

    Update: Sun 02/08/2026 12:03:26.88

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tsu/staff-portal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c263632aea10524290a891b1c64bec402a1b5f29',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tsu/staff-portal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c263632aea10524290a891b1c64bec402a1b5f29',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
